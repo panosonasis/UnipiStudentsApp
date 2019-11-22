f662c9c8b9c419f4c07bea5e8874972242afd135
ponasis <panos_onasis@hotmail.gr>
2019-11-22 16:36:29 +0200

    minor changes

--- UnipiStudents/MainWindow.xaml.cs
+++ UnipiStudents/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using UnipiStudents.Models;
 using UnipiStudents.ViewModels;
+using UnipiStudents.Views;
 
 namespace UnipiStudents
 {
@@ -50,6 +51,49 @@ namespace UnipiStudents
                 else if (thisViewModel.SelectedTreeItem.GetType() == typeof(Student))
                 {
                     MainContent.ContentTemplate = (DataTemplate)Resources["StudentViewTemplateData"];
+                }
+            }
+        }
+
+        private void AddGroup_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.ContentTemplate = (DataTemplate)Resources["GroupViewTemplate"];
+        }
+
+        private void AddLab_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.ContentTemplate = (DataTemplate)Resources["LabViewTemplate"];
+        }
+
+        private void AddCourse_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.ContentTemplate = (DataTemplate)Resources["CourseViewTemplate"];
+        }
+
+        private void StudentList_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var listview = sender as ListView;
+            var selected = listview.SelectedItem;
+            var test = FindVisualChildren<StudentView>(this);
+            if (selected != null)
+            {
+                MainContent.ContentTemplate = (DataTemplate)Resources["StudentViewTemplate"];
+            }
+        }
+
+        public IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+
+                    if (child != null && child is T)
+                        yield return (T)child;
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                        yield return childOfChild;
                 }
             }
         }
